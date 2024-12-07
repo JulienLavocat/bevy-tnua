76614a616c66bee31c0aff633b8f07bb6a3bf0cf
Julien Lavocat <julien.lavocat@gmail.com>
2024-12-07 14:52:12 +0100

    fix: remove rapier

--- demos/src/character_animating_systems/mod.rs
+++
@@ -1,1 +0,0 @@
-pub mod platformer_animating_systems;
--- demos/src/character_control_systems/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod info_dumpeing_systems;
-pub mod platformer_control_systems;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Dimensionality {
-    Dim2,
-    Dim3,
-}
